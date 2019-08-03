1ae37c2a27f1205b7a90256ba91fa48dfd533302
rohang13 <rohan13giri@gmail.com>
2019-08-03 16:40:13 +0530

    Commit Stock

---
+++ src/app/Search-Stock.ts
@@ -0,0 +1,7 @@
+import { Search } from './search';
+export const SEARCH : Search[] = [
+	{SearchElement:'Google', val:[1234,1452,1126.65,1520],SearchTime:''},
+	{SearchElement:'Microsoft',val:[1967.1,1972.5,1997.7,2002.11],SearchTime:''},
+	{SearchElement:'Amazon',val:[1465,2258,2369.5,2362],SearchTime:''},
+	{SearchElement:'Tata',val:[965.5,999,965.9,1003],SearchTime:''}
+];
